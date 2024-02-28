86b2f7a9a67dcccd6584403b6e3071c5e840f584
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-02-28 06:57:02 +0200

    I'm in the process of configuring webpack and other initial files, but a problem has arisen here. When I add code to the HTML file in the 'dist' folder, the content is not rendered in the browser.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import './style.css';
+import Family from './family.png';
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = document.createElement('img');
+  myIcon.src = Family;
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
